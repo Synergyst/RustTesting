c2786564dca5c221f410721eb0bee99d134ebe9d
Synergyst <na@example.com>
2023-02-05 20:20:50 -0600

    Add some structs to codebase

--- rust-test/src/main.rs
+++ rust-test/src/main.rs
@@ -12,10 +12,7 @@ use std::{thread, time};
 use std::io::{BufReader, Sink};
 use anyhow;
 use clap::Parser;
-use cpal::{
-    traits::{DeviceTrait, HostTrait, StreamTrait},
-    FromSample, Sample, SizedSample,
-};
+use cpal;
 use rodio;
 
 enum KeyInput {
@@ -34,17 +31,77 @@ enum KeyInput {
   VolumeUp,
   VolumeDown,
 }
-fn cycle_sound_file(snd_iter: &mut i32, actual_audio_file_list_size: i32, is_cycle_forward_down: bool, is_cycle_backward_down: bool, prev_cycle_forward_down: bool, prev_cycle_backward_down: bool) {
+
+enum OffSet {
+  Neg(usize),
+  Pos(usize),
+}
+
+struct KeyedInfoFile<'a> {
+  snd_iter: &'a mut usize,
+  actual_audio_file_list_size: usize,
+  is_cycle_forward_down: bool,
+  is_cycle_backward_down: bool,
+  prev_cycle_forward_down: bool,
+  prev_cycle_backward_down: bool,
+  audio_file_list: &'a mut Vec<String>
+}
+struct KeyedInfoFolder<'a> {
+  snd_dir_iter: &'a mut usize,
+  actual_audio_dir_list_size: usize,
+  is_cycle_forward_dir_down: bool,
+  is_cycle_backward_dir_down: bool,
+  prev_cycle_forward_dir_down: bool,
+  prev_cycle_backward_dir_down: bool,
+  sound_dir_list: &'a mut Vec<String>
+}
+
+fn bar(n: usize, offset: OffSet) -> Option<usize> {
+  match offset {
+      OffSet::Pos(offset) => n.checked_add(offset),
+      OffSet::Neg(offset) => n.checked_sub(offset),
+  }
+}
+fn hex_to_i32(hex: &str) -> i32 {
+  let without_prefix = hex.trim_start_matches("0x");
+  i32::from_str_radix(without_prefix, 16).unwrap()
+}
+fn hex_to_i64(hex: &str) -> i64 {
+  let without_prefix = hex.trim_start_matches("0x");
+  i64::from_str_radix(without_prefix, 16).unwrap()
+}
+/*fn cycle_sound_file(snd_iter: &mut i32, actual_audio_file_list_size: i32, is_cycle_forward_down: bool, is_cycle_backward_down: bool, prev_cycle_forward_down: bool, prev_cycle_backward_down: bool) {
   if is_cycle_forward_down && is_cycle_forward_down != prev_cycle_forward_down || is_cycle_backward_down && is_cycle_backward_down != prev_cycle_backward_down {
     if is_cycle_forward_down && is_cycle_forward_down != prev_cycle_forward_down {
       *snd_iter = (*snd_iter + 1) % (actual_audio_file_list_size);
     } else {
       *snd_iter = (*snd_iter + actual_audio_file_list_size) % (actual_audio_file_list_size + 1);
     }
-    //configureNextFile(audio_file_list[*snd_iter]);
+    configure_next_sound_file(audio_file_list[*snd_iter]);
+  }
+}*/
+fn cycle_sound_file(keyed_infos_file: &mut KeyedInfoFile) {
+  if keyed_infos_file.is_cycle_forward_down && keyed_infos_file.is_cycle_forward_down != keyed_infos_file.prev_cycle_forward_down || keyed_infos_file.is_cycle_backward_down && keyed_infos_file.is_cycle_backward_down != keyed_infos_file.prev_cycle_backward_down {
+    *keyed_infos_file.snd_iter = (*keyed_infos_file.snd_iter as isize + if keyed_infos_file.is_cycle_forward_down { 1 } else { -1 }) as usize % keyed_infos_file.actual_audio_file_list_size;
+    // CHANGEME?
+    if *keyed_infos_file.snd_iter < 0 {
+      *keyed_infos_file.snd_iter += keyed_infos_file.actual_audio_file_list_size;
+    }
+    configure_next_sound_file(&keyed_infos_file.audio_file_list[*keyed_infos_file.snd_iter as usize]);
   }
 }
-fn cycle_sound_library(snd_dir_iter: &mut i32, actual_audio_dir_list_size: i32, is_cycle_forward_dir_down: bool, is_cycle_backward_dir_down: bool, prev_cycle_forward_dir_down: bool, prev_cycle_backward_dir_down: bool) {
+fn cycle_sound_library(keyed_infos_folder: &mut KeyedInfoFolder) {
+  if keyed_infos_folder.is_cycle_forward_dir_down && keyed_infos_folder.is_cycle_forward_dir_down != keyed_infos_folder.prev_cycle_forward_dir_down || keyed_infos_folder.is_cycle_backward_dir_down && keyed_infos_folder.is_cycle_backward_dir_down != keyed_infos_folder.prev_cycle_backward_dir_down {
+    *keyed_infos_folder.snd_dir_iter = (*keyed_infos_folder.snd_dir_iter as isize + if keyed_infos_folder.is_cycle_forward_dir_down { 1 } else { -1 }) as usize;
+    if *keyed_infos_folder.snd_dir_iter >= keyed_infos_folder.actual_audio_dir_list_size {
+      *keyed_infos_folder.snd_dir_iter = 0;
+    } else {
+      *keyed_infos_folder.snd_dir_iter = keyed_infos_folder.actual_audio_dir_list_size - 1;
+    }
+    configure_next_sound_dir(&keyed_infos_folder.sound_dir_list[*keyed_infos_folder.snd_dir_iter]);
+  }
+}
+/*fn cycle_sound_library(snd_dir_iter: &mut isize, actual_audio_dir_list_size: isize, is_cycle_forward_dir_down: bool, is_cycle_backward_dir_down: bool, prev_cycle_forward_dir_down: bool, prev_cycle_backward_dir_down: bool, sound_dir_list: &mut Vec<String>) {
   if is_cycle_forward_dir_down && is_cycle_forward_dir_down != prev_cycle_forward_dir_down || is_cycle_backward_dir_down && prev_cycle_backward_dir_down != prev_cycle_backward_dir_down {
     *snd_dir_iter = *snd_dir_iter + if is_cycle_forward_dir_down { 1 } else { -1 };
     if *snd_dir_iter < 0 {
@@ -52,9 +109,9 @@ fn cycle_sound_library(snd_dir_iter: &mut i32, actual_audio_dir_list_size: i32, 
     } else if *snd_dir_iter > actual_audio_dir_list_size {
       *snd_dir_iter = 0;
     }
-    //configure_next_sound_dir(sound_dir_list[snd_dir_iter]);
+    configure_next_sound_dir(sound_dir_list[snd_dir_iter]);
   }
-}
+}*/
 fn get_key_state(r: KeyInput) -> bool {
   match r {
     KeyInput::NextSound | KeyInput::PrevSound => true,
@@ -72,7 +129,6 @@ fn list_files(dir: &str) -> Result<Vec<String>, std::io::Error> {
   }
   Ok(files)
 }
-
 fn list_folders(dir: &str) -> Result<Vec<String>, std::io::Error> {
   let paths = fs::read_dir(dir)?;
   let mut dirs = vec![];
@@ -84,28 +140,11 @@ fn list_folders(dir: &str) -> Result<Vec<String>, std::io::Error> {
   }
   Ok(dirs)
 }
-
-/*fn list_files(dir: &str) -> Vec<String> {
-  let paths = fs::read_dir(dir).unwrap();
-  let mut files = vec![];
-  for path in paths {
-    let path = path.unwrap().path();
-    if path.is_file() {
-      files.push(path.to_str().unwrap().to_owned());
-    }
-  }
-  files
+/*fn modify_folders(folders: &mut Vec<String>) {
+  // modify the `folders` value as needed
 }
-fn list_folders(dir: &str) -> Vec<String> {
-  let paths = fs::read_dir(dir).unwrap();
-  let mut dirs = vec![];
-  for path in paths {
-    let path = path.unwrap().path();
-    if path.is_dir() {
-      dirs.push(path.to_str().unwrap().to_owned());
-    }
-  }
-  dirs
+fn modify_files(files: &mut Vec<String>) {
+  // modify the `files` value as needed
 }*/
 fn listen_for_key_press(key: u32) -> bool {
   unsafe {
@@ -113,7 +152,7 @@ fn listen_for_key_press(key: u32) -> bool {
       key_state & 0x8000 != 0
   }
 }
-fn key_state_runner() {
+fn key_state_runner(keyed_infos_folder: &mut KeyedInfoFolder, keyed_infos_file: &mut KeyedInfoFile) {
   loop {
     let duration = time::Duration::from_millis(50);
     let now = time::Instant::now();
@@ -132,23 +171,24 @@ fn key_state_runner() {
     print!("\r                                                                                                                                                                        \r");
     print!("NUM 0: {:?}, NUM 1: {:?}, NUM 2: {:?}, NUM 3: {:?}, NUM 4: {:?}, NUM 5: {:?}, NUM 6: {:?}, NUM 7: {:?}, NUM 8: {:?}, NUM 9: {:?}",
     is_numpad0_pressed, is_numpad1_pressed, is_numpad2_pressed, is_numpad3_pressed, is_numpad4_pressed, is_numpad5_pressed, is_numpad6_pressed, is_numpad7_pressed, is_numpad8_pressed, is_numpad9_pressed);
+    if is_numpad0_pressed {
+      cycle_sound_library(keyed_infos_folder);
+    }
+    //
   }
 }
-
-/*fn modify_folders(folders: &mut Vec<String>) {
-  // modify the `folders` value as needed
+fn configure_next_sound_dir(sound_dir: &str) {
+  print!("\n{}\n", sound_dir);
 }
-
-fn modify_files(files: &mut Vec<String>) {
-  // modify the `files` value as needed
-}*/
-
+fn configure_next_sound_file(sound_file: &str) {
+  print!("\n{}\n", sound_file);
+}
 fn main() {
-  let folder_position: usize = 0;
+  let mut folder_position: usize = 0;
   let mut folder_position_max: usize = 0;
-  let file_position: usize = 0;
+  let mut file_position: usize = 0;
   let mut file_position_max: usize = 0;
-  let folders = list_folders("../synergyst-soundboard-util/sounds/").unwrap();
+  let mut folders = list_folders("../synergyst-soundboard-util/sounds/").unwrap();
   for folder in &folders {
     println!("{}: {}", folder_position_max, folder);
     folder_position_max += 1;
@@ -156,97 +196,28 @@ fn main() {
   println!("Count of folders: {}", folder_position_max);
   folder_position_max -= 1;
   let starting_dir = format!("{}{}", folders[folder_position], "/");
-  let files = list_files(starting_dir.as_str()).unwrap();
+  let mut files = list_files(starting_dir.as_str()).unwrap();
   for file in &files {
     println!("{}: {}", file_position_max, file);
     file_position_max += 1;
   }
   println!("Count of files: {}", file_position_max);
   file_position_max -= 1;
-  let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
+  // TODO: change audio_file_list to sound_file_list
+  let file_infos:KeyedInfoFile = KeyedInfoFile{snd_iter:&mut file_position,actual_audio_file_list_size:file_position_max,is_cycle_forward_down:false,is_cycle_backward_down:false,prev_cycle_forward_down:false,prev_cycle_backward_down:false,audio_file_list:&mut files};
+  //
+  let folder_infos:KeyedInfoFolder = KeyedInfoFolder{snd_dir_iter:&mut folder_position,actual_audio_dir_list_size:folder_position_max,is_cycle_forward_dir_down:false,is_cycle_backward_dir_down:false,prev_cycle_forward_dir_down:false,prev_cycle_backward_dir_down:false,sound_dir_list:&mut folders};
+  /*let (_stream, handle) = rodio::OutputStream::try_default().unwrap();
   let sink = rodio::Sink::try_new(&handle).unwrap();
   //
   let file = std::fs::File::open(&files[file_position]).unwrap();
   sink.append(rodio::Decoder::new(BufReader::new(file)).unwrap());
-  sink.sleep_until_end();
-
-  //modify_files(&mut files);
-  //modify_folders(&mut folders);
-
-  
-  //print!("{:?}", get_key_state(KeyInput::NextLibrary));
-  /*let files = list_files("./");
-  let folders = list_folders("./");
-  for file in files {
-    println!("File: {}", file);
-  }
-  for folder in folders {
-    println!("Folder: {}", folder);
-  }
-  let handle = thread::spawn(|| {
+  sink.sleep_until_end();*/
+  //
+  /*let handle = thread::spawn(|| {
     key_state_runner();
-});
-// rest of the main function code
-handle.join().unwrap();*/
-}
-
-
-fn hex_to_i32(hex: &str) -> i32 {
-  let without_prefix = hex.trim_start_matches("0x");
-  i32::from_str_radix(without_prefix, 16).unwrap()
-}
-fn hex_to_i64(hex: &str) -> i64 {
-  let without_prefix = hex.trim_start_matches("0x");
-  i64::from_str_radix(without_prefix, 16).unwrap()
-}
-fn udp_runner() -> std::io::Result<()> {
-  {
-    let socket = UdpSocket::bind("0.0.0.0:9999")?;
-    // Receives a single datagram message on the socket. If `buf` is too small to hold
-    // the message, it will be cut off.
-    let mut buf = [0; 64];
-    let (amt, src) = socket.recv_from(&mut buf)?;
-    // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-    let buf = &mut buf[..amt];
-    buf.reverse();
-    socket.send_to(buf, &src)?;
-  } // the socket is closed here
-  Ok(())
-}
-
-pub fn run<T>(device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error> where T: SizedSample + FromSample<f32>, {
-    let sample_rate = config.sample_rate.0 as f32;
-    let channels = config.channels as usize;
-
-    // Produce a sinusoid of maximum amplitude.
-    let mut sample_clock = 0f32;
-    let mut next_value = move || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * std::f32::consts::PI / sample_rate).sin()
-    };
-
-    let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-
-    let stream = device.build_output_stream(
-        config,
-        move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-            write_data(data, channels, &mut next_value)
-        },
-        err_fn,
-        None,
-    )?;
-    stream.play()?;
-
-    std::thread::sleep(std::time::Duration::from_millis(1000));
-
-    Ok(())
-}
-
-fn write_data<T>(output: &mut [T], channels: usize, next_sample: &mut dyn FnMut() -> f32) where T: Sample + FromSample<f32>, {
-    for frame in output.chunks_mut(channels) {
-        let value: T = T::from_sample(next_sample());
-        for sample in frame.iter_mut() {
-            *sample = value;
-        }
-    }
+  });
+  //print!("{:?}", get_key_state(KeyInput::NextLibrary));
+  // rest of the main function code
+  handle.join().unwrap();*/
 }
