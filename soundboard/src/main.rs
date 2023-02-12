baa89b4af37f5794e816e422c18abffda8db7f0a
Synergyst <na@example.com>
2023-02-12 14:14:58 -0600

    Update file

--- soundboard/src/main.rs
+++ soundboard/src/main.rs
@@ -30,48 +30,7 @@ use tui::{
 };
 use std::env;
 use std::ffi::OsStr;
-static VERSION: &str = "Version 0.1";
-struct App<'a> {
-  state: TableState,
-  items: Vec<Vec<&'a str>>,
-}
-impl<'a> App<'a> {
-  fn new() -> App<'a> {
-    App {
-      state: TableState::default(),
-      items: vec![
-        vec!["folder1", "filename1"],
-        vec!["folder2", "filename2"],
-      ],
-    }
-  }
-  pub fn next(&mut self) {
-    let i = match self.state.selected() {
-      Some(i) => {
-        if i >= self.items.len() - 1 {
-          0
-        } else {
-          i + 1
-        }
-      }
-      None => 0,
-    };
-    self.state.select(Some(i));
-  }
-  pub fn previous(&mut self) {
-    let i = match self.state.selected() {
-      Some(i) => {
-        if i == 0 {
-          self.items.len() - 1
-        } else {
-          i - 1
-        }
-      }
-      None => 0,
-    };
-    self.state.select(Some(i));
-  }
-}
+static VERSION: &str = "Version 0.2";
 enum KeyInput {
   NextSound,
   PrevSound,
@@ -286,11 +245,11 @@ fn main() {
     match &arg[..] {
       "-h" | "--help" => {
         help();
-        return;
+        std::process::exit(0);
       }
       "--version" => {
         println!("{} {}", prog().unwrap_or_default(), VERSION);
-        return;
+        std::process::exit(0);
       }
       "-n" | "--name" => {
         if let Some(arg_config) = args.next() {
@@ -388,26 +347,6 @@ fn main() {
   /*let result = enumerate_devices();
   println!("{:?}", result);*/
   //
-  // setup terminal
-  /*enable_raw_mode()?;
-  let mut stdout = io::stdout();
-  execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-  let backend = CrosstermBackend::new(stdout);
-  let mut terminal = Terminal::new(backend)?;
-  // create app and run it
-  let app = App::new();
-  let res = run_app(&mut terminal, app);
-  // restore terminal
-  disable_raw_mode()?;
-  execute!(
-    terminal.backend_mut(),
-    LeaveAlternateScreen,
-    DisableMouseCapture
-  )?;
-  terminal.show_cursor()?;
-  if let Err(err) = res {
-    println!("{:?}", err)
-  }*/
   //
   let mut config = DeviceConfig::new(DeviceType::Playback);
   config.playback_mut().set_format(miniaudio::Format::S16);
@@ -457,8 +396,16 @@ fn main() {
       }
     }
   }
+  /*let mut items: Vec<Vec<&str>> = vec![vec![]];
+  let binding = preffered_dev_name.to_string();
+  items[0].insert(0, &binding);
+  let binding = sound_dir_basename.to_string();
+  items[0].insert(1, &binding);
+  println!("{:#?}, {:#?}", items[0].get(0), items[0].get(1));*/
+  //
   let playback_devs_infos: PlaybackDevsInfo = PlaybackDevsInfo{playback_devs_id: dev_ids, playback_devs_name: dev_names};
   config.playback_mut().set_device_id(Some(playback_devs[preffered_dev_id].id().clone()));
+  //
   //
   loop {
     dev_infos.is_voice_down = listen_for_key_press(hex_to_u32(&voice_down_key_val.to_string()));
@@ -574,37 +521,4 @@ fn main() {
     folder_infos.prev_cycle_backward_dir_down = folder_infos.is_cycle_backward_dir_down;
     dev_infos.prev_voice_down = dev_infos.is_voice_down;
   }
-  //Ok(())
-}
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-  loop {
-    terminal.draw(|f| ui(f, &mut app))?;
-    if let Event::Key(key) = event::read()? {
-      match key.code {
-        KeyCode::Char('q') => return Ok(()),
-        KeyCode::Down => app.next(),
-        KeyCode::Up => app.previous(),
-        _ => {}
-      }
-    }
-  }
-}
-fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
-  let rects = Layout::default().constraints([Constraint::Percentage(100)].as_ref()).margin(5).split(f.size());
-  let selected_style = Style::default().add_modifier(Modifier::REVERSED);
-  let normal_style = Style::default().bg(Color::Blue);
-  //let header_cells = ["Header1", "Header2", "Header3"].iter().map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
-  let header_cells = ["CurrentFolder", "CurrentFile"].iter().map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
-  let header = Row::new(header_cells).style(normal_style).height(1).bottom_margin(1);
-  let rows = app.items.iter().map(|item| {
-    let height = item.iter().map(|content| content.chars().filter(|c| *c == '\n').count()).max().unwrap_or(0)+ 1;
-    let cells = item.iter().map(|c| Cell::from(*c));
-    Row::new(cells).height(height as u16).bottom_margin(1)
-  });
-  let t = Table::new(rows).header(header).block(Block::default().borders(Borders::NONE).title("Soundboard")).highlight_style(selected_style).highlight_symbol("-->").widths(&[
-    Constraint::Percentage(50),
-    Constraint::Length(30),
-    Constraint::Min(10),
-  ]);
-  f.render_stateful_widget(t, rects[0], &mut app.state);
 }
